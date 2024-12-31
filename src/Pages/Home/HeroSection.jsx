8c69112016af7b4ee5c9504dbaf05ffa1a6a43d4
AlexLi9132 <alexli@purdue.edu>
2024-12-31 01:56:58 -0500

    updated resume

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -12,7 +12,7 @@ export default function HeroSection() {
                     </p>
                 </div>
                 <a 
-    href="https://drive.google.com/file/d/1A7xIpn9q0OMB6f83_lE96fkl4G-UN4u_/view?usp=sharing" 
+    href="https://drive.google.com/file/d/1WN9yurjRmYxy0intdjTbQp5lisV_2-iF/view?usp=sharing" 
     className="btn btn-primary" 
     target="_blank" 
     rel="noopener noreferrer"
